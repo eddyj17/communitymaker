703fda56c7292085f393f9013699f356420efca3
Eddy Jusuf <eddy_jusuf@hotmail.com>
2016-03-19 11:44:19 +1100

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+  has_one :profile
+  has_many :events
+  has_many :posts
+  has_many :photos
+  has_many :memberships
+  has_many :groups, through: :memberships
+
+end
