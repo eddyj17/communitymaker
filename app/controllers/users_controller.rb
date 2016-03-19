e58d24b8e791895dbd47ae79b77c5f18275b8013
Eddy Jusuf <eddy_jusuf@hotmail.com>
2016-03-19 13:40:40 +1100

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.add_role :admin
     redirect_to :back
+  end
+  
 
 def destroy
   @user = User.find(params[:id])
